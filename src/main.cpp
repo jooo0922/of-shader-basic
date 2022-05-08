8eb357d9892ff191597dbac62bb811c21afe90c2
jooo0922 <2014102458@khu.ac.kr>
2022-05-09 00:15:29 +0900

    add: Draw first triangle mesh

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,13 @@
 
 //========================================================================
 int main( ){
-	ofSetupOpenGL(1024,768,OF_WINDOW);			// <-------- setup the GL context
-
+    // 아래 5줄은 초기의 main() 함수에서 원하는 버전의 OpenGL 을 사용하기 위해 수정해줘야 하는 부분들
+    ofGLWindowSettings glSettings; // glSettings 라는 변수에 윈도우 설정 관련 of 객체를 넣어줌
+    glSettings.setSize(1024, 768); // 결과물을 보여줄 윈도우 사이즈 설정
+    glSettings.windowMode = OF_WINDOW; // 윈도우 모드는 OF_FULLSCREEN(크기 조절 안되는 전체화면모드) / OF_WINDOW(크기 조절 가능한 실행창) 가 있는 거 같음.
+    glSettings.setGLVersion(4, 1); // OpenGL 4.1 버전을 쓰려면 (4, 1) 이렇게 정수값을 전달해줘야 함.
+    ofCreateWindow(glSettings); // 설정이 변경된 윈도우 설정 of 객체를 ofCreateWindow() 함수에 전달해주면 실행창(윈도우)를 열어줌
+    
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
 	// pass in width and height too:
--- src/ofApp.h
+++ src/ofApp.h
@@ -20,5 +20,5 @@ class ofApp : public ofBaseApp{
 		void windowResized(int w, int h);
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
-		
+    ofMesh triangle; // ofApp의 draw() 함수에서도 triangle 에 접근하려고 헤더파일에 해당 변수를 선언함.
 };
