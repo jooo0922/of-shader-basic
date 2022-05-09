e5218cbdf55c6cb15ab2f709228087bf68ab666f
jooo0922 <2014102458@khu.ac.kr>
2022-05-09 23:53:12 +0900

    add: Use ofShader for shader loading and binding

--- src/ofApp.h
+++ src/ofApp.h
@@ -21,4 +21,5 @@ class ofApp : public ofBaseApp{
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
     ofMesh triangle; // ofApp의 draw() 함수에서도 triangle 에 접근하려고 헤더파일에 해당 변수를 선언함.
+    ofShader shader; // bin/data 디렉토리에 작성한 셰이더 파일들을 ofApp의 setup() 및 draw() 함수에서 로드 및 바인딩하기 위해 필요한 ofShader 객체를 담은 변수를 헤더파일에 선언함.
 };
